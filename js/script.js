a82dddceb28729ab2c6f333278235e1c89ad5e48
Yamilah Atallah <yami@yamilah.com>
2015-05-10 22:18:11 -0400

    Added event handlers for menu visibilities and filtration, some preliminary effects.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#iconWrapper").click(function() {
+        $("aside nav").slideToggle();
+    });
+
+    $("nav li").click(function() {
+        $(this).toggleClass("selected");
+        var dataType = $(this).data("type");
+
+        $("[data-"+dataType+"]").slideToggle();
+    });
+});
